8c06bb14f09730b957ab30491e9bc8602774221d
aslamcontact <aslam.mycontact@gmail.com>
2024-07-27 22:46:47 +0530

    set up projects

---
+++ src/main/java/com/aslam/mycontact/prompt_engine_opt/PromptEngineOptApplication.java
@@ -0,0 +1,13 @@
+package com.aslam.mycontact.prompt_engine_opt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromptEngineOptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromptEngineOptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aslam/mycontact/prompt_engine_opt/PromptEngineOptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aslam.mycontact.prompt_engine_opt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromptEngineOptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
