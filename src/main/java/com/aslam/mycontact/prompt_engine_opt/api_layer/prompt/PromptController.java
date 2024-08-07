076cde36ad1f736bf440c25137fa0407cabe17b4
aslamcontact <aslam.mycontact@gmail.com>
2024-08-07 15:38:49 +0530

    fix conflict

--- src/main/java/com/aslam/mycontact/prompt_engine_opt/api_layer/prompt/PromptController.java
+++ src/main/java/com/aslam/mycontact/prompt_engine_opt/api_layer/prompt/PromptController.java
@@ -1,7 +1,6 @@
 package com.aslam.mycontact.prompt_engine_opt.api_layer.prompt;
 
-
-
+import com.aslam.mycontact.prompt_engine_opt.bussiness_layer.application.app_prompt.PromptPayload;
 import com.aslam.mycontact.prompt_engine_opt.bussiness_layer.prompt.PromptService;
 import com.aslam.mycontact.prompt_engine_opt.bussiness_layer.prompt.prompt_generator.response_parsers.prompt_template.PromptTemplatePayload;
 import org.springframework.http.HttpStatus;
@@ -12,18 +11,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
 @RequestMapping("/api/v1/template")
-
 public class PromptController {
- private final PromptService promptService;
+    private final PromptService promptService;
 
     public PromptController(PromptService promptService) {
         this.promptService = promptService;
     }
-
- 
-
     @PostMapping("/prompt")
     ResponseEntity<PromptTemplatePayload> generatePrompt(@RequestBody String prompt)
     {
