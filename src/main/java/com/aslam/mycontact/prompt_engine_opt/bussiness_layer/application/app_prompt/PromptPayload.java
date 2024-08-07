99bc1003f4ae9d984e64b7a083be70db4a6ddf74
aslamcontact <aslam.mycontact@gmail.com>
2024-08-07 14:59:52 +0530

    add validator for payload and rename prompt to appPrompt

--- src/main/java/com/aslam/mycontact/prompt_engine_opt/bussiness_layer/application/app_prompt/PromptPayload.java
+++
@@ -1,7 +0,0 @@
-package com.aslam.mycontact.prompt_engine_opt.bussiness_layer.application.app_prompt;
-
-public record PromptPayload(String promptName,
-                            String template,
-                            String examplePrompt,
-                            String outputFormat) {
-}
--- src/main/java/com/aslam/mycontact/prompt_engine_opt/bussiness_layer/application/app_task/AppTaskPayload.java
+++ src/main/java/com/aslam/mycontact/prompt_engine_opt/bussiness_layer/application/app_task/AppTaskPayload.java
@@ -1,6 +1,14 @@
 package com.aslam.mycontact.prompt_engine_opt.bussiness_layer.application.app_task;
 
-public record AppTaskPayload(String taskName,
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record AppTaskPayload(
+                             @NotNull(message ="taskName required" )
+                             @NotBlank(message = "taskName can't be empty")
+                             String taskName,
+                             @NotNull(message ="taskTask required" )
+                             @NotBlank(message = "taskTask can't be empty")
                              String aboutTask
                              ) {
 }
